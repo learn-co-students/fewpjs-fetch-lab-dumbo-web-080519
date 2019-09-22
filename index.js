52eb147af87b50ea5b3f5048f339a58fdd0ea6fd
deryatanriverdi88 <deryatanriverdi_@hotmail.com>
2019-09-21 20:42:53 -0400

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 function fetchBooks() {
+ fetch("https://anapioficeandfire.com/api/books")
+ .then(resp => resp.json())
+ .then(renderBooks(jsonObject));
+}
 
+function renderBooks(jsonObject) {
+  const main = document.querySelector('main')
+  jsonObject.forEach(book => {
+    const h2 = document.createElement('h2')
+    h2.innerHTML = `<h2>${book.name}</h2>`
+    main.appendChild(h2)
+  })
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  fetchBooks()
+})
+function fetchBooks() {
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(function(response) {return response.json()})
+  .then(function(json) {renderBooks(json)})
 }
 
 function renderBooks(json) {
